403cf9ea697fd616251b9f0606ea58265a2fee45
alpha7179 <alpha_loading7179@naver.com>
2025-11-19 12:12:11 +0900

    [Setting] Proto 개발 세팅

---
+++ XRPBL_MindDiver_Unity/Assets/2. Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 1. 싱글톤 인스턴스
+    public static GameManager Instance { get; private set; }
+
+    // 2. Awake() 함수 구현
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬이 바뀌어도 파괴되지 않음
+        }
+        else
+        {
+            Destroy(gameObject); // 이미 인스턴스가 있다면 이 오브젝트는 파괴
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
